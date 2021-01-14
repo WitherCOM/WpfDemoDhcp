c3250b01ec9716814d8a88f2dec44b3018f26161
WitherCOM <nemeth.csaba.tibor@gmail.com>
2021-01-14 17:44:08 +0100

    Add project files.

---
+++ DhcpProba/Model/Dhcp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DhcpProba.Model
+{
+    class Dhcp : INotifyPropertyChanged
+    {
+
+        
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ DhcpProba/ViewModel/DhcpViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DhcpProba.ViewModel
+{
+    class DhcpViewModel
+    {
+
+    }
+
+    class Updater : ICommand
+    {
+        #region ICommand Members  
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            //Your Code  
+        }
+        #endregion
+    }
+}
